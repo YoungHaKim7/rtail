f73840dd5d8f0c896a83edbed1870146eb6c9250
GYoung <ytok1108@aol.com>
2024-10-29 23:20:56 +0900

    Result Error impl

--- src/result_error.rs
+++ src/result_error.rs
@@ -1,4 +1,4 @@
-use std::result;
+use std::{error::Error, fmt, result};
 
 use crate::{HasArg, Name};
 
@@ -26,4 +26,33 @@ pub enum Fail {
 enum Optval {
     Val(String),
     Given,
+}
+
+impl Error for Fail {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        None
+    }
+
+    fn description(&self) -> &str {
+        "description() is deprecated; use Display"
+    }
+
+    fn cause(&self) -> Option<&dyn Error> {
+        self.source()
+    }
+
+    // fn provide<'a>(&'a self, request: &mut std::error::Request<'a>) {}
+}
+
+impl fmt::Display for Fail {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        use Fail::*;
+        match *self {
+            ArgumentMissing(ref nm) => write!(f, "Argument to option '{}' missing", *nm),
+            UnrecognizedOption(ref nm) => write!(f, "Unrecognized option: '{}'", *nm),
+            OptionMissing(ref nm) => write!(f, "Required option '{}' missing", *nm),
+            OptionDuplicated(ref nm) => write!(f, "Option '{}' given more than once", *nm),
+            UnexpectedArgument(ref nm) => write!(f, "Option '{}' does not take an argument", *nm),
+        }
+    }
 }
